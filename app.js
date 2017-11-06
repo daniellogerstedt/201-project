fb58f881430c3f7e48fbd117aac7f0ce082b40fd
kbperez <kbpgperez@hotmail.com>
2017-11-06 13:26:02 -0800

    finish username input

--- app.js
+++ app.js
@@ -9,17 +9,22 @@ var newUser; //For first time users
 
 
 
-//Check for localStorage
+//Check localStorage for a returning user
 function getUser(){
   if (localStorage.user){
     user = localStorage.user;
+    document.getElementById('alias').value=user;
   }else{
     //If new visitor, display element for name.
     newUser = document.getElementById('welcome');
     //  Also, hide other elements
     newUser.setAttribute('class', 'active');
     newUser.addEventListener('submit', save);
+    var main = document.getElementsByTagName('main')[0];
+    main.style.display = 'none';
+    newUser.style.display = 'block';
   }
+
   return user;
 }
 
@@ -30,6 +35,9 @@ function save(e){
   localStorage.user = user;  //Store in localStorage
   //Hide user input box
   document.getElementById('welcome').setAttribute('class', 'inactive');
+  var main = document.getElementsByTagName('main')[0];
+  main.style.display = 'block';
+  newUser.style.display = 'none';    document.getElementById('alias').value=user;
 }
 
 getUser();
