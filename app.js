be60c84b25d1813c10220dcf84ef86ad90d11c5e
kbperez <kbpgperez@hotmail.com>
2017-11-06 16:35:52 -0800

    forms and buttons

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
 //Variable declaration
 var user;  //Check for returning users
 var newUser; //For first time users
+var password=document.getElementById('key');//Password for encrypting and decrypting messages
+var message=document.getElementById('large_box'); //Message to be encrypted
 
 //Helper functions
-
-
 
 //Check localStorage for a returning user
 function getUser(){
@@ -24,11 +24,10 @@ function getUser(){
     main.style.display = 'none';
     newUser.style.display = 'block';
   }
-
   return user;
 }
 
-//Function to check for input
+//Function to save username
 function save(e){
   event.preventDefault(); //Prevent reload
   user = e.target.user.value; //Get user name
@@ -40,4 +39,68 @@ function save(e){
   newUser.style.display = 'none';    document.getElementById('alias').value=user;
 }
 
+//Function to check which button was selected
+function button(e){
+  e.preventDefault();
+
+  if (e.target.innerHTML==='Destroy Message'){
+    message.value='';
+    password.value='';
+
+  } else if (e.target.innerHTML==='Encrypt'){
+    if (!checker()){
+      alert('Please enter missing fields.');
+      return;
+      }
+      var level = document.getElementById('difficulty').value;
+      if (level==='value1'){
+        var output = confidentialEncrypt(message.value, password.value);
+        document.getElementById('read_only_message').innerHTML=output;
+      }else if (level==='value2'){
+        var output = secretEncrypt(message.value, password.value);
+        document.getElementById('read_only_message').innerHTML=output;
+      }else if (level==='value3'){
+        var output = topSecretEncrypt(message.value, password.value);
+        document.getElementById('read_only_message').innerHTML=output;
+      }else if (level==='value4'){
+        alert('NSA Encryption Detected.');
+      }
+
+  } else if (e.target.innerHTML==='Decrypt'){
+    if (!checker()){
+      alert('Please enter missing fields.');
+      return;
+      }
+      var level = document.getElementById('difficulty').value;
+    if (level==='value1'){
+      var output = confidentialDecrypt(message.value, password.value);
+      document.getElementById('read_only_message').innerHTML=output;
+    }else if (level==='value2'){
+      var output = secretDecrypt(message.value, password.value);
+      document.getElementById('read_only_message').innerHTML=output;
+    }else if (level==='value3'){
+      var output = topSecretDecrypt(message.value, password.value);
+      document.getElementById('read_only_message').innerHTML=output;
+    }else if (level==='value4'){
+      alert('NSA Encryption Detected.');
+    }
+  }
+
+  }
+
+function checker(){
+  if (password.value==='') {
+    return false;
+  }
+  if (message.value==='') {
+    return false;
+  }
+  return true;
+}
+
 getUser();
+
+//Adding event listeners on buttons
+document.getElementById('encode').addEventListener('click', button);
+document.getElementById('decode').addEventListener('click', button);
+document.getElementById('clearbtn').addEventListener('click', button);
