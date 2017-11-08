9f678b6a172b362ced64be4398204fefac23f8b7
JEREMY PEARSON <JPJAZZY@HOTMAIL.COM>
2017-11-08 14:24:20 -0800

    fixed overlay and listener

--- js/nsa.js
+++ js/nsa.js
@@ -2,15 +2,28 @@
 
 var toggle = document.getElementById('difficulty');
 var overlay = document.getElementById('overlay');
+var timeoutVar;
 
-toggle.addEventListener('change', function() {
+toggle.addEventListener('change', displayNSA);
+
+function displayNSA() {
   console.log('change:', this.value);
   if (this.value === 'value4') {
-    overlay.setAttribute('class', 'active'); 
-      overlay.style.display = 'block';
+    overlay.setAttribute('class', 'active');
+    document.getElementsByTagName('main')[0].style.display = 'none';
+    document.getElementsByTagName('header')[0].style.display = 'none';
+    document.getElementsByTagName('footer')[0].style.display = 'none';
+    overlay.style.display = 'block';
+    timeoutVar = window.setTimeout(displayPage, 10000);
   }
-});
+}
 
-overlay.addEventListener('click', function() {
-  this.style.display = 'none';
-});
+overlay.addEventListener('click', displayPage);
+function displayPage() {
+
+  document.getElementsByTagName('main')[0].style.display = 'block';
+  document.getElementsByTagName('header')[0].style.display = 'block';
+  document.getElementsByTagName('footer')[0].style.display = 'block';
+  overlay.style.display = 'none';
+  clearTimeout(timeoutVar);
+}
